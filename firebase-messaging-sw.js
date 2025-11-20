f35aea59e2472ab2727dc01eec32be75675eb66a
Studora <146615146+studora@users.noreply.github.com>
2025-11-20 17:10:04 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,10 +5,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-comp
 const firebaseConfig = {
   apiKey: "AIzaSyDKTmbeR8vxWOimsera1WmC6a5mZc_Ewkc",
   authDomain: "closeddoor-58ac5.firebaseapp.com",
+  databaseURL: "https://closeddoor-58ac5-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "closeddoor-58ac5",
   storageBucket: "closeddoor-58ac5.firebasestorage.app",
   messagingSenderId: "330800003542",
-  appId: "1:330800003542:web:2d02cf0d6eb01d5bdfcc77"
+  appId: "1:330800003542:web:2d02cf0d6eb01d5bdfcc77",
+  measurementId: "G-B1DJWK271Y"
 };
 
 firebase.initializeApp(firebaseConfig);
